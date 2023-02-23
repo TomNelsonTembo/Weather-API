dc91977cdd8e64c82f7d02c8c3d243f203bd19a7
Tom Nelson Tembo <85808556+TomNelsonTembo@users.noreply.github.com>
2023-02-23 12:36:13 +0100

    Add files via upload

---
+++ WeatherAPI Project/WeatherAPI Project/Database.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.Data;
+
+namespace WeatherAPI_Project
+{
+    class Database
+    {
+        public SQLiteConnection connection;
+        public Database()
+        {
+
+            connection = new SQLiteConnection("Data source = WeatherAPIDatabase.sqlite3"); //connection to the datasource
+            if (!File.Exists("./WeatherAPIDatabase.sqlite3"))
+            {
+                SQLiteConnection.CreateFile("WeatherAPIDatabase.sqlite3");
+                MessageBox.Show("Database Created");
+            }
+        }
+        //class Meathod that opens connection to Sqlite Database
+        public void Openconnection()
+        {
+            if (connection.State != ConnectionState.Open)
+            {
+                connection.Open();
+            }
+            else
+            {
+                //Do Nothing
+            }
+
+        }
+        //Classe method that closes connection to the database
+        public void Closeconnection()
+        {
+            if (connection.State != ConnectionState.Closed)
+            {
+                connection.Close();
+            }
+            else
+            {
+                //Do nothing
+            }
+        }
+    }
+}
